76c4e20bde1e3d2601111ca8998be7edf72a8d2c
Alexandr Vinogradov <izum709@.com>
2023-04-19 16:37:26 +0300

    Add task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+string revnumber = "";
+for (int index = number.Length - 1; index >= 0; index--)
+{
+    revnumber =  revnumber + number[index];
+}
+
+if (number == revnumber)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
